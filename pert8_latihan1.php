5fb4898899aee700483198af5e83d360937ca43f
Shofierikmaulana <62019573+shofierik-maulana@users.noreply.github.com>
2021-04-17 15:53:09 +0700

    Add files via upload

---
+++ pert8_latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+function basic($argument) 
+ { 
+ echo $argument; 
+ } 
+ basic('hello world!'); // outputs 'hello world!' 
+  ?>
+</body>
+</html>
